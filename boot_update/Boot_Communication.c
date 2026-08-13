df0c745d88b15437a4376473ba270034dd34ff89
琼 <3556227509@qq.com>
2026-08-13 20:28:53 +0800

    功能调整
    Removed unused variables and optimized data handling.

--- boot_update/Boot_Communication.c
+++ boot_update/Boot_Communication.c
@@ -16,7 +16,6 @@ extern uint32_t current_address;
 //SPI处理标志位
 extern uint8_t spi_receive_flag;
 
-uint8_t pData[BOOT_COMMUNICATION_SIZE];
 //接收数据包
 uint8_t Data[BOOT_COMMUNICATION_SIZE];
 //接收数据大小
@@ -26,7 +25,7 @@ void Communication_Receive(UART_HandleTypeDef *huart)
 {
     __HAL_UART_CLEAR_OREFLAG(huart);
     __HAL_UART_CLEAR_IDLEFLAG(huart);
-    HAL_UARTEx_ReceiveToIdle_IT(huart, pData, BOOT_COMMUNICATION_SIZE);
+    HAL_UARTEx_ReceiveToIdle_IT(huart, Data, BOOT_COMMUNICATION_SIZE);
 }
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
@@ -35,19 +34,15 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
     {
         if(update_flag==1)
         {
-            memcpy(Data,pData,Size);
             spi_receive_flag=1;
             Data_Size=Size;
         }
-        else if(update_flag==0 && strncmp((char *)pData, "UPDATE", 6) == 0)
+        else if(update_flag==0 && strncmp((char *)Data, "UPDATE", 6) == 0)
         {
             //接收到更新命令，进入接收状态
             update_flag = 1;
-            
         }
 		__HAL_UART_CLEAR_OREFLAG(huart);
-		__HAL_UART_CLEAR_IDLEFLAG(huart);
-		HAL_UARTEx_ReceiveToIdle_IT(huart, pData, BOOT_COMMUNICATION_SIZE); 
-//        memset(pData, 0, BOOT_COMMUNICATION_SIZE); //清除未使用的缓冲区
+		HAL_UARTEx_ReceiveToIdle_IT(huart, Data, BOOT_COMMUNICATION_SIZE); 
     }
 }
