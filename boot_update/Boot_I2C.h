80849510b49ddb96a7415fa4e962af562ac39609
琼 <3556227509@qq.com>
2026-08-13 20:29:41 +0800

    功能调整

--- boot_update/Boot_I2C.h
+++ boot_update/Boot_I2C.h
@@ -5,10 +5,11 @@
 #define AT24C20_SIZE        256
 #define AT24C20_PAGE_SIZE   8
 #define AT24C20_ADDRESS     0xA0
-//APP地址
+//NEW_APP是否可用标志位地址,AT24C02
 #define UPDATE_FLAG_ADDRESS 0x00
-//NEW_APP地址
-#define UPDATE_FLAG_NEW_ADDRESS 0x02
+//存放Bug标志位,>6则程序正常,更新APP为NEW_APP程序,等于0则NEW_APP异常,进入APP(在boot里减一,在NEW_APP里加二)
+#define UPDATE_BUG_ADDRESS 0x02
+//地址对应的值
 #define UPDATE_FLAG_OK      0x01
 #define UPDATE_FLAG_FAIL    0x00
 
@@ -22,5 +23,6 @@ void Boot_I2C_erase(void);
 //写入更新标志
 void Boot_I2C_writeflag(uint8_t add,uint8_t key);
 uint8_t Boot_I2C_readkey(void);
+uint8_t Boot_I2C_bug(void);
 
 #endif
